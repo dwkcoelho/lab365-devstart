7bc8b0d169324a093c0fe3cd076b042231db8a33
dwkcoelho <dwkcoelho@gmail.com>
2022-12-09 21:26:26 -0300

    Create dados-atletas.js

---
+++ dados-atletas.js
@@ -0,0 +1,78 @@
+class Atleta {
+  constructor(nome, idade, peso, altura, notas) {
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.altura = altura;
+    this.notas = notas;
+  }
+  calculaCategoria() {
+    if (this.idade >= 9 && this.idade <= 11) {
+      return "Infantil";
+    } else if (this.idade === 12 || this.idade === 13) {
+      return "Juvenil";
+    } else if (this.idade === 14 || this.idade === 15) {
+      return "Intermediário";
+    } else if (this.idade >= 16 && this.idade <= 30) {
+      return "Adulto";
+    } else {
+      return "Sem categoria";
+    }
+  }
+  calculaIMC() {
+    return this.peso / (this.altura * this.altura).toFixed(2);
+  }
+  calculaMediaValida() {
+    let notasGerais = this.notas;
+    let notasOrganizadas = notasGerais.sort(function comparaNumeros(a, b) {
+      return a - b;
+    });
+
+    let notasComputadas = notasOrganizadas.slice(1, 4);
+    let somaNotasComputadas = notasComputadas.reduce(function (total, atual) {
+      return total + atual;
+    }, 0);
+
+    let media = (somaNotasComputadas / notasComputadas.length).toFixed(2);
+    return media;
+  }
+  obtemNomeAtleta() {
+    return this.nome;
+  }
+  obtemIdadeAtleta() {
+    return this.idade;
+  }
+  obtemPesoAtleta() {
+    return this.peso;
+  }
+  obtemAlturaAtleta() {
+    return this.altura;
+  }
+  obtemNotasAtleta() {
+    return this.notas;
+  }
+  obtemCategoria() {
+    return this.calculaCategoria();
+  }
+  obtemIMC() {
+    return this.calculaIMC();
+  }
+  obtemMediaValida() {
+    return this.calculaMediaValida();
+  }
+}
+const atleta = new Atleta(
+  "Cesar Abascal",
+  30,
+  80,
+  1.7,
+  [10, 9.34, 8.42, 10, 7.88]
+);
+console.log("Nome: " + atleta.obtemNomeAtleta());
+console.log("Idade: " + atleta.obtemIdadeAtleta());
+console.log("Peso: " + atleta.obtemPesoAtleta());
+console.log("Altura: " + atleta.obtemAlturaAtleta());
+console.log("Notas: " + atleta.obtemNotasAtleta());
+console.log("Categoria: " + atleta.obtemCategoria());
+console.log("IMC: " + atleta.obtemIMC());
+console.log("Média válida: " + atleta.obtemMediaValida());
